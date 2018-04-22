17c177ef085cb5269c0a80f897e813bde87521a8
Andrey Klyukin <a.klyukin@fbs.com>
2018-04-23 01:44:06 +0300

    fix

--- rabbit-test-worker.pb.go
+++ rabbit-test-worker.pb.go
@@ -100,64 +100,64 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// Client API for Worker service
+// Client API for Node service
 
-type WorkerClient interface {
+type NodeClient interface {
 	CheckStatus(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*NodeStatusResponse, error)
 }
 
-type workerClient struct {
+type nodeClient struct {
 	cc *grpc.ClientConn
 }
 
-func NewWorkerClient(cc *grpc.ClientConn) WorkerClient {
-	return &workerClient{cc}
+func NewNodeClient(cc *grpc.ClientConn) NodeClient {
+	return &nodeClient{cc}
 }
 
-func (c *workerClient) CheckStatus(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*NodeStatusResponse, error) {
+func (c *nodeClient) CheckStatus(ctx context.Context, in *Empty, opts ...grpc.CallOption) (*NodeStatusResponse, error) {
 	out := new(NodeStatusResponse)
-	err := grpc.Invoke(ctx, "/rabbittest.Worker/CheckStatus", in, out, c.cc, opts...)
+	err := grpc.Invoke(ctx, "/rabbittest.Node/CheckStatus", in, out, c.cc, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// Server API for Worker service
+// Server API for Node service
 
-type WorkerServer interface {
+type NodeServer interface {
 	CheckStatus(context.Context, *Empty) (*NodeStatusResponse, error)
 }
 
-func RegisterWorkerServer(s *grpc.Server, srv WorkerServer) {
-	s.RegisterService(&_Worker_serviceDesc, srv)
+func RegisterNodeServer(s *grpc.Server, srv NodeServer) {
+	s.RegisterService(&_Node_serviceDesc, srv)
 }
 
-func _Worker_CheckStatus_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Node_CheckStatus_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Empty)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(WorkerServer).CheckStatus(ctx, in)
+		return srv.(NodeServer).CheckStatus(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/rabbittest.Worker/CheckStatus",
+		FullMethod: "/rabbittest.Node/CheckStatus",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(WorkerServer).CheckStatus(ctx, req.(*Empty))
+		return srv.(NodeServer).CheckStatus(ctx, req.(*Empty))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _Worker_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "rabbittest.Worker",
-	HandlerType: (*WorkerServer)(nil),
+var _Node_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "rabbittest.Node",
+	HandlerType: (*NodeServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "CheckStatus",
-			Handler:    _Worker_CheckStatus_Handler,
+			Handler:    _Node_CheckStatus_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -167,7 +167,7 @@ var _Worker_serviceDesc = grpc.ServiceDesc{
 func init() { proto.RegisterFile("rabbit-test-worker.proto", fileDescriptor0) }
 
 var fileDescriptor0 = []byte{
-	// 188 bytes of a gzipped FileDescriptorProto
+	// 186 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x92, 0x28, 0x4a, 0x4c, 0x4a,
 	0xca, 0x2c, 0xd1, 0x2d, 0x49, 0x2d, 0x2e, 0xd1, 0x2d, 0xcf, 0x2f, 0xca, 0x4e, 0x2d, 0xd2, 0x2b,
 	0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x82, 0xc8, 0x80, 0x24, 0x94, 0x1a, 0x18, 0xb9, 0x84, 0xfc,
@@ -176,8 +176,8 @@ var fileDescriptor0 = []byte{
 	0x7a, 0x08, 0x3d, 0x7a, 0x98, 0xea, 0xf5, 0xa0, 0x5c, 0xa8, 0x26, 0x25, 0x6d, 0x2e, 0x36, 0x88,
 	0x88, 0x10, 0x27, 0x17, 0x6b, 0x90, 0xab, 0xa3, 0x4b, 0xa4, 0x00, 0x03, 0x88, 0x19, 0xec, 0xe3,
 	0xea, 0x1a, 0x20, 0xc0, 0x28, 0xc4, 0xcd, 0xc5, 0x1e, 0xee, 0x1f, 0xe4, 0xed, 0xe9, 0xe7, 0x2e,
-	0xc0, 0xa4, 0xc4, 0xce, 0xc5, 0xea, 0x9a, 0x5b, 0x50, 0x52, 0x69, 0xe4, 0xc3, 0xc5, 0x16, 0x0e,
-	0x76, 0xa7, 0x90, 0x13, 0x17, 0xb7, 0x73, 0x46, 0x6a, 0x72, 0x36, 0xd4, 0x10, 0x41, 0x64, 0xdb,
-	0xc1, 0x6a, 0xa5, 0xe4, 0xf0, 0x3b, 0x48, 0x89, 0x21, 0x89, 0x0d, 0xec, 0x59, 0x63, 0x40, 0x00,
-	0x00, 0x00, 0xff, 0xff, 0xbe, 0x65, 0x72, 0x17, 0x08, 0x01, 0x00, 0x00,
+	0xc0, 0xa4, 0xc4, 0xce, 0xc5, 0xea, 0x9a, 0x5b, 0x50, 0x52, 0x69, 0xe4, 0xc5, 0xc5, 0x02, 0x32,
+	0x5a, 0xc8, 0x89, 0x8b, 0xdb, 0x39, 0x23, 0x35, 0x39, 0x1b, 0x6a, 0x84, 0x20, 0xb2, 0xdd, 0x60,
+	0x95, 0x52, 0x72, 0xf8, 0x9d, 0xa3, 0xc4, 0x90, 0xc4, 0x06, 0xf6, 0xaa, 0x31, 0x20, 0x00, 0x00,
+	0xff, 0xff, 0x85, 0xf7, 0x82, 0x84, 0x06, 0x01, 0x00, 0x00,
 }
